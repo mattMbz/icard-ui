2d59f3cdb90a6430a8ced85b9361b79996f89b7a
mattMbz <matias.mbz@gmail.com>
2024-03-13 09:51:27 -0300

    Creating JWT for Django-Rest-Framework. '/login/auth' path has been created by the Backend

--- src/components/Admin/LoginForm/LoginForm.jsx
+++ src/components/Admin/LoginForm/LoginForm.jsx
@@ -1,13 +1,51 @@
 import React from "react";
 import { Button, Form } from "semantic-ui-react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import "./LoginForm.scss";
 
 export function LoginForm() {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: (formValue) => {
+      console.log('Sending Email ...')
+      console.log(formValue)
+    }
+  });
+
   return (
-    <Form className="login-form-admin">
-      <Form.Input name="email" placeholder="Email" />
-      <Form.Input name="password" type="password" placeholder="Password" />
+    <Form className="login-form-admin" onSubmit={formik.handleSubmit}>
+      <Form.Input
+        name="email"
+        placeholder="Email"
+        value={formik.values.email}
+        onChange={formik.handleChange}
+        error={formik.errors.email}
+      />
+      <Form.Input
+        name="password"
+        type="password"
+        placeholder="Password"
+        value={formik.values.password}
+        onChange={formik.handleChange}
+        error={formik.errors.password}
+      />
       <Button type="submit" content="Log in Now!" primary fluid />
     </Form>
   );
+}
+
+function initialValues() {
+  return {
+    email: "",
+    password: "",
+  };
+}
+
+function validationSchema() {
+  return {
+    email: Yup.string().email(true).required(true),
+    password: Yup.string().required(true)
+  };
 }
